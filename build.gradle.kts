cc8c21d54caba57239990aafa615627bff4d68a4
Oleksii Ignachuk <oignachuk@cybervisiontech.com>
2018-12-17 01:01:59 +0200

    rough experiment on pypi repo creation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,25 @@
 import com.avast.gradle.dockercompose.ComposeExtension
 import com.avast.gradle.dockercompose.DockerComposePlugin
 import de.undercouch.gradle.tasks.download.Download
+import org.apache.http.Header
+import org.apache.http.HttpHeaders
+import org.apache.http.HttpHost
+import org.apache.http.HttpStatus
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.client.protocol.HttpClientContext
+import org.apache.http.entity.FileEntity
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.auth.BasicScheme
+import org.apache.http.impl.client.BasicAuthCache
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.apache.http.impl.client.HttpClientBuilder
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestStackTraceFilter
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.sonatype.nexus.httpclient.internal.NexusHttpRoutePlanner
 
 buildscript {
     repositories {
@@ -11,6 +27,7 @@ buildscript {
     }
     dependencies {
         classpath("com.avast.gradle:gradle-docker-compose-plugin:0.8.12")
+        classpath("org.sonatype.nexus:nexus-rest-client:3.14.0-04")
     }
 }
 
@@ -36,7 +53,7 @@ val isOsLinux = System.getProperty("os.name").toLowerCase().contains("linux")
 var dockerComposeVersion = "1.23.2"
 val dockerComposeCacheDir = "$rootDir/.gradle/docker-compose/"
 val dockerComposeScript = "$dockerComposeCacheDir/$dockerComposeVersion/docker-compose.sh"
-val dockerComposeCommand = if (isOsLinux && project.hasProperty("dockerComposeVersion")) dockerComposeScript else "docker-compose"
+val dockerComposeCommand = if (isOsLinux) dockerComposeScript else "docker-compose"
 
 val spekVersion = "2.0.0-rc.1"
 
@@ -67,6 +84,8 @@ dependencies {
 configure<ComposeExtension> {
     projectName = project.name
     executable = dockerComposeCommand
+    dockerComposeWorkingDirectory = projectDir.absolutePath
+    useComposeFiles = listOf("docker-compose.yml")
     removeVolumes = true
     removeOrphans = true
     waitForTcpPorts = true
@@ -113,7 +132,52 @@ tasks {
         doLast { file(dockerComposeScript).setExecutable(true) }
     }
 
+
     "composeUp" {
         dependsOn(downloadDockerCompose)
+    }
+
+    val createPythonRepo by creating {
+        group = "build setup"
+        dependsOn(":composeUp")
+        doLast {
+
+            val credentials = UsernamePasswordCredentials("admin", "admin123")
+            val credentialsProvider = BasicCredentialsProvider()
+            val targetHost = HttpHost("localhost", 8081, "http")
+
+            credentialsProvider.setCredentials(
+                    AuthScope(targetHost.getHostName(), targetHost.getPort()),
+                    credentials)
+            val authCache = BasicAuthCache()
+            val basicAuth = BasicScheme()
+            authCache.put(targetHost, basicAuth)
+            val context = HttpClientContext.create()
+            context.setCredentialsProvider(credentialsProvider)
+            context.setAuthCache(authCache)
+
+
+            val client = HttpClientBuilder.create()
+                    .setDefaultCredentialsProvider(credentialsProvider)
+                    .build()
+
+            val loadScriptRequest = HttpPost("/service/rest/v1/script")
+            loadScriptRequest.setHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+            loadScriptRequest.entity = FileEntity(File(projectDir, "pypi.json"))
+
+            val loadScriptResponse = client.execute(targetHost, loadScriptRequest, context)
+            val loadStatusCode = loadScriptResponse.statusLine.statusCode
+            if (loadStatusCode != HttpStatus.SC_NO_CONTENT) {
+                throw GradleScriptException("Script load failed : $loadScriptResponse", null)
+            }
+
+            val runScriptRequest = HttpPost("/service/rest/v1/script/pypi/run")
+            runScriptRequest.setHeader(HttpHeaders.CONTENT_TYPE, "text/plain")
+            val runScriptResponse = client.execute(targetHost, runScriptRequest, context)
+            val runStatusCode = runScriptResponse.statusLine.statusCode
+            if (runStatusCode != HttpStatus.SC_OK) {
+                throw GradleScriptException("Script run failed : $runScriptResponse", null)
+            }
+        }
     }
 }
